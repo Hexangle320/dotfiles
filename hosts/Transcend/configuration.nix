98a1751d519e978c7504c9c36fdd586a8096df5e
Hexangle320 <116458789+Hexangle320@users.noreply.github.com>
2025-07-02 12:03:31 -0500

    note for myself

--- hosts/Transcend/configuration.nix
+++ hosts/Transcend/configuration.nix
@@ -10,7 +10,9 @@
     ];
 
   networking.hostName = "Transcend"; # Define your hostname.
-   #networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+  # when changing hostname run "rm -rf ~/.config/google-chrome/Singleton*" 
+  # allows google chrome to run
+  #networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
 
   # Configure network proxy if necessary
   # networking.proxy.default = "http://user:password@proxy:port/";
