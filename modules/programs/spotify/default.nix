e47c003c1fe7db1c8731b214e818c184b30c1b4b
Hexangle320 <116458789+Hexangle320@users.noreply.github.com>
2025-07-01 22:17:56 -0500

    change spotify version to 1.2.22

--- modules/programs/spotify/default.nix
+++ modules/programs/spotify/default.nix
@@ -1,10 +1,25 @@
-{pkgs, inputs, ...}:
+{pkgs, inputs, lib, ...}:
   let
+    system = "x86_64-linux";
+    pkgs = import (builtins.fetchGit {
+         name = "spotify-1.2.22";
+         url = "https://github.com/NixOS/nixpkgs/";
+         ref = "refs/heads/nixpkgs-unstable";
+         rev = "459104f841356362bfb9ce1c788c1d42846b2454";
+     }) {
+      inherit system;
+      # for some what ever f*ucking reason "nixpkgs.config.allowUnfree = true;"
+      # doesn't work
+      config = { allowUnfree = true; };
+     };
+
+     oldSpotify = pkgs.spotify;
     spicePkgs = inputs.spicetify-nix.legacyPackages.${pkgs.stdenv.system};
   in
   {
   programs.spicetify = {
     enable = true;
+    spotifyPackage = oldSpotify;
     enabledExtensions = with spicePkgs.extensions; [
       adblockify
       hidePodcasts
