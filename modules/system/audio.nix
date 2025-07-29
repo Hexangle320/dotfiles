acb25db305da3d2b49d6925c9e83102df56ca701
Hexangle320 <116458789+Hexangle320@users.noreply.github.com>
2025-07-29 15:51:35 -0500

    fix audio popping

--- modules/system/audio.nix
+++ modules/system/audio.nix
@@ -6,5 +6,31 @@
     alsa.enable = true;
     alsa.support32Bit = true;
     pulse.enable = true;
+    wireplumber.extraConfig."99-disable-suspend" = {
+      "monitor.alsa.rules" = [
+        {
+          matches = [
+            # Uncomment to disable suspend for all sinks
+            # {
+            #   "node.name" = "~alsa_input.*";
+            # }
+            # {
+            #   "node.name" = "~alsa_output.*";
+            # }
+            {
+              "node.name" = "alsa_output.pci-0000_00_1f.3-platform-sof_sdw.HiFi__Headphones__sink";
+            }
+          ];
+          actions = {
+            update-props = {
+              "session.suspend-timeout-seconds" = 0;
+              "node.always-process" = true;
+              "dither.method" = "wannamaker3";
+              "dither.noise" = 1;
+            };
+          };
+        }
+      ];
+    };
   };
 }
