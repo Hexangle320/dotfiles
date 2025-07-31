ebfefda743420f46b424bedf3a9c3383c7ee5ece
Hexangle320 <116458789+Hexangle320@users.noreply.github.com>
2025-07-30 22:10:57 -0500

    enable polkit

--- hosts/Transcend/configuration.nix
+++ hosts/Transcend/configuration.nix
@@ -35,6 +35,8 @@
     criticalPowerAction = "PowerOff";
   };
 
+  security.polkit.enable = true;
+
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
   # programs.mtr.enable = true;
