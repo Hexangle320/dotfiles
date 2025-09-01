318ca6af24150e8a8d669c5bd4d5ba8dbe680b9d
Hexangle320 <116458789+Hexangle320@users.noreply.github.com>
2025-08-31 21:50:57 -0500

    add aliases to fish

--- users/dots/fish/config.fish
+++ users/dots/fish/config.fish
@@ -1,3 +1,9 @@
+alias glog="git log --graph --abbrev-commit --decorate --format=format:'%C(bold blue)%h%C(reset) - %C(bold green)(%ar)%C(reset) %C(white)%s%C(reset) %C(dim white)- %an%C(reset)%C(auto)%d%C(reset)' --all"
+
+alias system="sudo nixos-rebuild --log-format bar --no-reexec --file ~/nixos -A nixosConfigurations.$prompt_hostname"
+
+alias ls="eza --icons --group-directories-first -1"
+
 set sponge_purge_only_on_exit true
 set fish_greeting
 set fish_cursor_insert line blink
