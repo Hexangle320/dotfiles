895b612c2664017a78932b6dad5ac5ccc9a636fa
Hexangle320 <116458789+Hexangle320@users.noreply.github.com>
2026-04-26 18:26:05 -0500

    feat: remove firewall

--- modules/system/networking.nix
+++ modules/system/networking.nix
@@ -1,18 +1,11 @@
 {...}: {
   networking = {
-    nftables.enable = true;
     networkmanager = {
       enable = true;
       wifi = {
         powersave = false;
         macAddress = "random";
       };
-    };
-
-    firewall = {
-      enable = false;
-      allowedTCPPortRanges = [];
-      allowedUDPPortRanges = [];
     };
   };
   hardware.bluetooth = {
